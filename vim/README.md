08bfd97d26fe05fc25f1b286cbe2f6af4e399661
Scott Allen <scott@plum.io>
2016-06-21 15:09:53 -0400

    Scripted the setup of vim.

--- vim/README.md
+++ vim/README.md
@@ -1,4 +1,1 @@
-From within the bundle dir:
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone https://github.com/davidhalter/jedi-vim.git
+Run `./setup-vim.sh`
---
+++ vim/setup-vim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ./.vimrc ~/
+mkdir ~/.vim
+
+cp -r autoload ~/.vim
+cp -r bundle ~/.vim
+cp -r colors ~/.vim
+cp -r ftplugin ~/.vim
+
+cd ~/.vim/bundle/
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/davidhalter/jedi-vim.git
